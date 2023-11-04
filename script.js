1022e40baf476592bf2638a3430326bfe93e1cf3
vgpatil2103 <patilvaibhav2108@gmail.com>
2023-11-04 16:05:15 +0530

    Search and filter functionality Done

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const recipeCardsContainer = document.querySelector('.recipe-cards');
 
+const cardlist = document.querySelectorAll(".cardlist");
+
+
+const aboveFourCheckbox = document.getElementById("abovefour");
+const belowFourCheckbox = document.getElementById("belowfour");
+const searchInput = document.getElementById("search-tab");
+
+const leftmenu=document.querySelector(".left-menu");
+const mergemenu=document.querySelector(".merged-menu");
+const container=document.querySelector(".section");
+
+
+function showMenu(){
+    mergemenu.style.display="block";
+    
+    leftmenu.style.display="none";
+    
+}
+
+leftmenu.addEventListener("click",showMenu);
+
 
 const recipeData = [
     {
@@ -124,6 +145,7 @@ const recipeData = [
     }
 ];
 
+///to add the images dynaically
 recipeData.forEach(recipe => {
     const card = document.createElement('div');
     card.className = 'cardlist';
@@ -176,3 +198,149 @@ recipeData.forEach(recipe => {
 
     recipeCardsContainer.appendChild(card);
 });
+
+
+// Set the "Show All" toggle as the default
+const showAllButton = document.querySelector(".all-recipes");
+showAllButton.style.backgroundColor = "lightblue"; // Highlight the "Show All" button
+let currentFilter = "all"; // Current filter, initially set to "all"
+
+// Function to update the displayed recipes based on filters
+function updateDisplayedRecipes() {
+  const filteredRecipes = recipeData.filter((recipe) => {
+    if (currentFilter === "veg" && recipe.type === "non-veg") {
+      return false; // Hide non-veg recipes when "Show Only Veg" is selected
+    }
+    if (currentFilter === "non-veg" && recipe.type === "veg") {
+      return false; // Hide veg recipes when "Show Only Non-Veg" is selected
+    }
+    if (
+      currentFilter === "aboveFour" &&
+      (recipe.rating < 4.5 || recipe.rating === undefined)
+    ) {
+      return false; // Hide recipes with a rating below 4.5
+    }
+    if (currentFilter === "belowFour" && recipe.rating >= 4) {
+      return false; // Hide recipes with a rating of 4 or above
+    }
+    if (
+      searchInput.value.trim() !== "" &&
+      !recipe.name.toLowerCase().includes(searchInput.value.trim().toLowerCase())
+    ) {
+      return false; // Hide recipes that don't match the search query
+    }
+    return true;
+  });
+
+  // Clear the existing recipe cards
+  recipeCardsContainer.innerHTML = "";
+
+  // Create and append recipe cards for the filtered recipes
+  filteredRecipes.forEach((recipe) => {
+    // Create recipe cards (same code as before)
+    // ...recipeData.forEach(recipe => {
+    const card = document.createElement('div');
+    card.className = 'cardlist';
+
+    const cardImage = document.createElement('div');
+    cardImage.className = 'card-image';
+    const image = document.createElement('img');
+    image.src = recipe.imageSrc;
+    image.className = 'dishimg';
+    cardImage.appendChild(image);
+
+    const vegNonVeg = document.createElement('div');
+    vegNonVeg.className = 'veg-nonveg';
+    const type = document.createElement('p');
+    type.id = 'type';
+    type.textContent = recipe.type === 'veg' ? 'Veg' : 'Non-Veg';
+    vegNonVeg.appendChild(type);
+
+    const dishName = document.createElement('div');
+    dishName.className = 'dish-name';
+    const nameOfDish = document.createElement('p');
+    nameOfDish.id = 'name-of-dish';
+    nameOfDish.textContent = recipe.name;
+    dishName.appendChild(nameOfDish);
+
+    const starRating = document.createElement('div');
+    starRating.className = 'star-rating';
+    const starImage = document.createElement('img');
+    starImage.id = 'star';
+    starImage.src = 'Star.svg';
+    const starRatingValue = document.createElement('p');
+    starRatingValue.id = 'star-rating';
+    starRatingValue.textContent = recipe.rating;
+    starRating.appendChild(starImage);
+    starRating.appendChild(starRatingValue);
+
+    dishName.appendChild(starRating);
+
+    const time = document.createElement('div');
+    time.className = 'time';
+    const timeForDish = document.createElement('p');
+    timeForDish.id = 'time-for-dish';
+    timeForDish.textContent = recipe.time;
+    time.appendChild(timeForDish);
+
+    card.appendChild(cardImage);
+    card.appendChild(vegNonVeg);
+    card.appendChild(dishName);
+    card.appendChild(time);
+
+    recipeCardsContainer.appendChild(card);
+});
+
+  
+}
+
+// Event listeners for filter buttons
+showAllButton.addEventListener("click", () => {
+  currentFilter = "all";
+  showAllButton.style.backgroundColor = "lightblue";
+  aboveFourCheckbox.checked = false;
+  belowFourCheckbox.checked = false;
+  updateDisplayedRecipes();
+});
+
+document.querySelector(".Veg-recipes").addEventListener("click", () => {
+  currentFilter = "veg";
+  showAllButton.style.backgroundColor = "";
+  aboveFourCheckbox.checked = false;
+  belowFourCheckbox.checked = false;
+  updateDisplayedRecipes();
+});
+
+document.querySelector(".Non-Veg-recipes").addEventListener("click", () => {
+  currentFilter = "non-veg";
+  showAllButton.style.backgroundColor = "";
+  aboveFourCheckbox.checked = false;
+  belowFourCheckbox.checked = false;
+  updateDisplayedRecipes();
+});
+
+aboveFourCheckbox.addEventListener("change", () => {
+  if (aboveFourCheckbox.checked) {
+    currentFilter = "aboveFour";
+    showAllButton.style.backgroundColor = "";
+    belowFourCheckbox.checked = false;
+    updateDisplayedRecipes();
+  }
+});
+
+belowFourCheckbox.addEventListener("change", () => {
+  if (belowFourCheckbox.checked) {
+    currentFilter = "belowFour";
+    showAllButton.style.backgroundColor = "";
+    aboveFourCheckbox.checked = false;
+    updateDisplayedRecipes();
+  }
+});
+
+// Event listener for the search input
+searchInput.addEventListener("input", () => {
+  updateDisplayedRecipes();
+});
+
+// Initial load with default filters
+updateDisplayedRecipes();
